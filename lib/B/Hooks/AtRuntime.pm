950e50c87d370580b87b90ec5297b1831b6805d8
Ben Morrow <ben@morrow.me.uk>
2012-02-12 05:12:42 +0000

    Account for multiple simultaneous BEGINs.
    It's actually slightly tricky to create these: BEGIN { BEGIN { } } isn't
    enough, since the inner is over and gone before the outer starts
    running; however
    
        BEGIN { eval q{ BEGIN { } } }
    
    and (more importantly) the equivalent with require instead of eval *are*
    enough.

--- lib/B/Hooks/AtRuntime.pm
+++ lib/B/Hooks/AtRuntime.pm
@@ -13,36 +13,60 @@ XSLoader::load __PACKAGE__, $VERSION;
 our @EXPORT = "at_runtime";
 our @EXPORT_OK = qw/at_runtime lex_stuff/;
 
-my $Hooks;
+my @Hooks;
 
-sub clear {
+sub replace_run {
+    my ($new) = @_;
+    local $" = "][";
+    warn "REPLACE [@{$new || []}]\n";
+
     # By deleting the stash entry we ensure the only ref to the glob is
     # through the optree it was compiled into. This means that if that
     # optree is ever freed, the glob will disappear along with @hooks
     # and anything closed over by the user's callbacks.
     delete $B::Hooks::AtRuntime::{run};
-    $Hooks = undef;
+
+    no strict "refs";
+    $new and *{"run"} = $new->[1];
+}
+
+sub clear {
+    my ($depth) = @_;
+    local $" = "][";
+    no warnings "uninitialized";
+    warn "CLEAR: [$depth] [@{$Hooks[$depth] || []}]\n";
+    $Hooks[$depth] = undef;
+    replace_run $Hooks[$depth - 1];
 }
 
 sub at_runtime (&) {
     my ($cv) = @_;
 
-    unless ($Hooks) {
-        # This must be a symref, so we get a fresh glob each time.
-        my $gv = do { no strict "refs"; \*{"run"} };
+    local $" = "][";
 
+    my $depth = count_BEGINs();
+    warn "DEPTH: [$depth]\n";
+
+    my $hk;
+    unless ($hk = $Hooks[$depth]) {
         # Close over an array of callbacks so we don't need to keep
         # stuffing text into the buffer.
-        $Hooks = \my @hooks;
-        *$gv = subname "run", sub { $_->() for @hooks };
+        my @hooks;
+        $hk = $Hooks[$depth] = [ 
+            \@hooks, 
+            subname "run", sub { $_->() for @hooks } 
+        ];
+        warn "ALLOC: [@{$hk}]\n";
+        replace_run $hk;
 
         # This must be all on one line, so we don't mess up perl's idea
         # of the current line number.
         lex_stuff("B::Hooks::AtRuntime::run();" .
-            "BEGIN{B::Hooks::AtRuntime::clear()}");
+            "BEGIN{B::Hooks::AtRuntime::clear($depth)}");
     }
 
-    push @$Hooks, $cv;
+    warn "PUSH: [@$hk]\n";
+    push @{$$hk[0]}, $cv;
 }
 
 1;
--- lib/B/Hooks/AtRuntime.xs
+++ lib/B/Hooks/AtRuntime.xs
@@ -2,6 +2,39 @@
 #include <perl.h>
 #include <XSUB.h>
 
+#ifndef caller_cx
+#define caller_cx(c, p)         MY_caller_cx(aTHX_ c, p)
+
+/* Since we're only looking for BEGINs, we can skip most of the
+ * subtleties of the real caller_cx and not worry about returning a
+ * whole lot of extra frames that aren't subs.
+ */
+static const PERL_CONTEXT *
+MY_caller_cx(pTHX_ I32 count, const PERL_CONTEXT **dbcxp)
+{
+    register I32 cxix = cxstack_ix;
+    register const PERL_CONTEXT *ccstack = cxstack;
+    const PERL_SI *top_si = PL_curstackinfo;
+
+    for (;;) {
+	/* we may be in a higher stacklevel, so dig down deeper */
+	while (cxix < 0 && top_si->si_type != PERLSI_MAIN) {
+	    top_si = top_si->si_prev;
+	    ccstack = top_si->si_cxstack;
+	    cxix = top_si->si_cxix;
+	}
+	if (cxix < 0)
+	    return NULL;
+	if (!count--)
+	    break;
+	cxix = cxix - 1;
+    }
+
+    return &ccstack[cxix];
+}
+
+#endif
+
 MODULE = B::Hooks::AtRuntime  PACKAGE = B::Hooks::AtRuntime
 
 void
@@ -9,3 +42,22 @@ lex_stuff (s)
         SV *s
     CODE:
         lex_stuff_sv(s, 0);
+
+UV
+count_BEGINs ()
+    PREINIT:
+        I32 c = 0;
+        const PERL_CONTEXT *cx;
+        const CV *cxcv;
+    CODE:
+        RETVAL = 0;
+        while (cx = caller_cx(c++, NULL)) {
+            if (CxTYPE(cx) == CXt_SUB   &&
+                (cxcv = cx->blk_sub.cv) &&
+                CvSPECIAL(cxcv)         &&
+                strEQ(GvNAME(CvGV(cxcv)), "BEGIN")
+            )
+                RETVAL++;
+        }
+    OUTPUT:
+        RETVAL
